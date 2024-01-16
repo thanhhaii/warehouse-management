2745042485a075f2dc5f0b64f303846d74266386
thanhhaii <hailamnguyenthanh@gmail.com>
2024-01-17 01:44:13 +0700

    Init project structure

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,15 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: [
+    'dist',
+    '.eslintrc.cjs',
+  ],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
@@ -14,5 +17,5 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
-  },
+  }
 }
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+      './src/**/*.{ts,tsx,html,js,jsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
