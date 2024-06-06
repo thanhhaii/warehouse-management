d52997e2e5de4137277f754dfedb1fc9742bb943
thanhhaii <hailamnguyenthanh@gmail.com>
2024-06-06 22:07:07 +0700

    feat(login): validate token expired

--- src/states/selectors/authSelector.ts
+++ src/states/selectors/authSelector.ts
@@ -1,11 +1,22 @@
 import { createSelector } from "@reduxjs/toolkit";
 import { IRootState } from "../rootReducer";
+import stringHelpers from "@/helpers/stringHelper";
+import dayjs from "dayjs";
 
 const getAuthReducer = (appStates: IRootState) => appStates.authReducer;
 
 export const selectUserIsSignedIn = createSelector(
     getAuthReducer,
     (state) => {        
-        return state.isSignedIn;
+        if(!state.token || !state.isSignedIn){
+            return false;
+        }
+
+        const data = stringHelpers.getDataFormToken(state.token);
+        if(dayjs().valueOf() < (data?.exp * 1000)) {
+            return true;
+        }
+
+        return false;
     }
 );
